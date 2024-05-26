a546e72d64bd7d73eb5ad7a86cf204529c3b861e
sohail shaik <shaiksohail2332000@gmail.com>
2024-05-25 21:48:00 -0700

    initial message

---
+++ magic8Ball/magic8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magic8Ball
+//
+//  Created by sohail shaik on 5/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
