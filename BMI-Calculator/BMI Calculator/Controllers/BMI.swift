c6eb60265968552a81289ab2d64b3fee5c716db9
sohail shaik <shaiksohail2332000@gmail.com>
2024-06-10 14:21:11 -0700

    Add: BMI calculator project

---
+++ BMI-Calculator/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by sohail shaik on 6/10/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value : String?
+    let advice: String?
+    let color : UIColor?
+}
---
+++ BMI-Calculator/BMI Calculator/Views/ResultsUIScreen.swift
@@ -0,0 +1,13 @@
+//
+//  ResultsUIScreen.swift
+//  BMI Calculator
+//
+//  Created by sohail shaik on 6/10/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultsUIScreen: NSObject {
+
+}
